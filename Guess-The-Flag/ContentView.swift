80fac3f71c0274e3fae4b45e7cc7a05f3099885c
alula-z <azeruesenay14@gmail.com>
2025-08-24 23:16:00 -0400

    Guessing game complete

--- Guess-The-Flag/ContentView.swift
+++ Guess-The-Flag/ContentView.swift
@@ -8,15 +8,99 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var questionsSeen = 0
+    @State private var finishedQuestions = false
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State private var showScore = false
+    @State private var score = 0
+    @State private var scoreTitle = ""
+    @State private var correctAnswer = Int.random(in: 0...2)
+    func flagTapped(_ number: Int){
+        if number == correctAnswer{
+            scoreTitle = "Correct"
+            
+            score += 1
+        }else{
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+            if score > 0 {
+                score = score - 1
+            }else{
+                score = 0
+            }
         }
-        .padding()
+        showScore = true
+        questionsSeen += 1
+        if questionsSeen >= 8{
+            finishedQuestions = true
+        }
     }
+    func askQuestion(){
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    func resetGame(){
+        score = 0
+        questionsSeen = 0
+    }
+    var body: some View {
+        ZStack{
+            RadialGradient(stops:[
+                .init(color:Color(red:0.1,green:0.2,blue: 0.45), location: 0.3),
+                .init(color:Color(red:0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 500)
+            .ignoresSafeArea()
+            
+            VStack{
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack(spacing: 15){
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.weight(.heavy))
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    
+                    ForEach(0..<3){number in
+                        Button{
+                            flagTapped(number)
+                        }label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                .shadow(radius: 5)
+                        }
+                    }
+                }
+                .frame(maxWidth:.infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 30))
+                Spacer()
+                Spacer()
+                Text("Your score is \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+            }
+            .padding()
+            
+        }
+        .alert(scoreTitle, isPresented: $showScore){
+            Button("Continue", action: askQuestion)
+        }message: {
+            Text("Your score is \(score)")
+        }
+        .alert("",isPresented: $finishedQuestions){
+            Button("Reset Game",action: resetGame)
+        }message:{
+            Text("Your final score is \(score)")
+        }
+        
+    }
+    
 }
 
 #Preview {
